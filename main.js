16bf5e47c2fa36ad6914c392e6fe147e22833303
Emmanuel Agbonyeme <nereus4luv@gmail.com>
2023-08-01 15:24:35 +0100

    Update

--- main.js
+++ main.js
@@ -8,7 +8,7 @@
 
 let correctAnswersContainer = document.querySelector('.correctAnswers');
 let questionContainer = document.querySelector('.question');
-  const apiUrl = "https://opentdb.com/api.php?amount=10&category=21&difficulty=medium&type=multiple";
+  const apiUrl = "https://opentdb.com/api.php?amount=10";
 fetch(apiUrl)
 .then((response) => {
   return response.json()
@@ -114,6 +114,9 @@ fetch(apiUrl)
             // Assign the counter value to the result span
           let resultOutput = document.querySelector('.score');
           resultOutput.textContent = counter;
+          // Assign Overall score 
+        let overallScore = document.querySelector('.overall-score');
+        overallScore.textContent = questionNumber;
 
       }
           });
